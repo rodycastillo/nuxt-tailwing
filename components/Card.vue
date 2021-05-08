14d7064348b0ef25afb4e97fa430e3a613e53b99
Carlito <carlorody1000@gmail.com>
2021-05-07 21:55:19 -0500

    WIP

---
+++ components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container h-full p-1">
+    <div
+      class="p-5 border-l-4 border-blue-500 bg-blue-100 text-blue-500 text-sm font-bold"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" name="Rody" value="" />
+    <card title="Title" v-for="item in 4"></card>
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+export default {
+  components: { Card }
+};
+</script>
